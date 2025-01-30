edb8e8c1d7512c5e94195bc9ead0b2913e392a55
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-01-30 10:46:55 -0300

    implementando Lista Suspensa

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../CampoTexto/CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
   return (
     <section className="formulario">
       <form>
@@ -11,6 +23,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   );
--- src/components/ListaSuspensa/index.js
+++ src/components/ListaSuspensa/index.js
@@ -1,5 +1,14 @@
 import './ListaSuspensa.css';
 
-const ListaSuspensa = () => {}
+const ListaSuspensa = (props) => {
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+      <select>
+        {props.itens.map((item) => <option key={item}>{item}</option>)}
+      </select>
+    </div>
+  );
+}
 
 export default ListaSuspensa;
