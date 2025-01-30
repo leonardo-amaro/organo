5f57a58e7f45fd26c391e437afb6e4d266591fcf
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-01-30 11:29:15 -0300

    validando formulario

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -13,16 +13,21 @@ const Formulario = () => {
     'UX e Design',
     'Mobile',
     'Inovação e Gestão'
-  ]
+  ];
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log('Form submetido!');
+  }
 
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>
           Preencha os dados para criar o card do colaborador
         </h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
         <ListaSuspensa label="Time" itens={times} />
         <Botao>
