43a289727a310b2257e19a1be31028a734e11f73
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-04 10:20:37 -0300

    elevando a lista de times

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -2,8 +2,10 @@ import './Time.css';
 
 const Time = (props) => {
   return (
-    <section className='time'>
-      <h3>{props.nome}</h3>
+    <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+      <h3 style={{ borderColor: props.corPrimaria }}>
+        {props.nome}
+      </h3>
     </section>
   );
 }
