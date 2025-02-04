59087cffce94bb84740b3987ebab75e21aaed74e
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-04 11:06:12 -0300

    implementando componente Colaborador

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,5 +1,17 @@
 import './Colaborador.css';
 
-const Colaborador = () => {}
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src='https://github.com/leonardo-amaro.png' alt='Imagem do colaborador'/>
+      </div>
+      <div className='rodape'>
+        <h4>Leonardo Silva</h4>
+        <h5>Desenvolvedor</h5>
+      </div>
+    </div>
+  );
+}
 
 export default Colaborador;
