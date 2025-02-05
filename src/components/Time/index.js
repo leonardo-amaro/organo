6edf60baca6ec7aa6cad9feec85b0628c8acec29
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-05 08:08:33 -0300

    corrigido erro de digitação (colaboradores)

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -4,7 +4,7 @@ import './Time.css';
 const Time = (props) => {
   return (
     // Renderização condicional (se não houver colaborador na lista, o componente não é renderizado)
-    props.colaborador.length > 0 && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+    props.colaboradores.length > 0 && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>
         {props.nome}
       </h3>
