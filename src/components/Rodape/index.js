ebab2038114855a50574d64f5a147213b843cf32
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-05 08:20:16 -0300

    implementando componente Rodape

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -1,5 +1,21 @@
 import './Rodape.css';
 
-const Rodape = () => {}
+const Rodape = () => {
+  return (
+    <footer>
+      <div>
+        <img src='/imagens/fb.png' alt='Logo Facebook' />
+        <img src='/imagens/tw.png' alt='Logo Twitter' />
+        <img src='/imagens/ig.png' alt='Logo Instagram' />
+      </div>
+      <div>
+        <img src='/imagens/logo.png' alt='Logo da plataforma Organo' />
+      </div>
+      <div>
+        <p>Criado por Alura</p>
+      </div>
+    </footer>
+  )
+}
 
 export default Rodape;
