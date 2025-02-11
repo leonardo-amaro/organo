55fb0f75ea2bcfb7f3aa495d384e2874cb450372
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-11 10:01:03 -0300

    add branch componentes para testes

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,17 +1,15 @@
-import './Colaborador.css';
+import './colaborador.css'
 
-const Colaborador = ({ nome, cargo, corDeFundo }) => {
-  return (
-    <div className='colaborador'>
-      <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
-        <img src='https://github.com/leonardo-amaro.png' alt='Imagem do colaborador'/>
-      </div>
-      <div className='rodape'>
-        <h4>{nome}</h4>
-        <h5>{cargo}</h5>
-      </div>
-    </div>
-  );
+const Colaborador = ({ colaborador, corDeFundo }) => {
+    return (<div className="colaborador">
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
+        </div>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+        </div>
+    </div>)
 }
 
-export default Colaborador;
+export default Colaborador
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,23 +1,17 @@
-import Colaborador from '../Colaborador';
-import './Time.css';
+import Colaborador from '../Colaborador'
+import './time.css'
 
-const Time = (props) => {
-  return (
-    // Renderização condicional (se não houver colaborador na lista, o componente não é renderizado)
-    props.colaboradores.length > 0 && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
-      <h3 style={{ borderColor: props.corPrimaria }}>
-        {props.nome}
-      </h3>
-      <div className='colaboradores'>
-        {props.colaboradores.map((colaborador) => <Colaborador
-          key={colaborador.nome}
-          nome={colaborador.nome}
-          cargo={colaborador.cargo}
-          corDeFundo={props.corPrimaria}
-        />)}
-      </div>
-    </section>
-  );
+const Time = ({ time, colaboradores }) => {
+    return (
+
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+            </div>
+        </section>
+
+    )
 }
 
-export default Time;
+export default Time
