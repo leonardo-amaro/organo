64b43293c2758cc794b6ef22155d83bc3c2b1a3d
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-14 11:16:54 -0300

    add botão deletar colaborador

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,14 +1,14 @@
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, deletarColaborador }) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={deletarColaborador} />
                 })}
             </div>
         </section>
