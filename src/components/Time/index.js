93f90713fe7e9ceaf996fb7f2a57f3b1eca0b518
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-18 11:00:48 -0300

    refatorando uso da cor

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -4,12 +4,12 @@ import './time.css'
 const Time = ({ time, colaboradores, deletarColaborador, mudaCor }) => {
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <input type='color' className='muda-cor' value={time.corSecundaria} onChange={(evento) => mudaCor(evento.target.value, time.nome)} />
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)' }}>
+            <input type='color' className='muda-cor' value={time.cor} onChange={(evento) => mudaCor(evento.target.value, time.nome)} />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={deletarColaborador} />
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={deletarColaborador} />
                 })}
             </div>
         </section>
