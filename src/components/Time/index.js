5322b66066cc823c90e27d0df338c679f5445016
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-02-18 11:09:07 -0300

    convertendo hex para rgba

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,10 +1,11 @@
+import hexToRgba from 'hex-to-rgba'
 import Colaborador from '../Colaborador'
 import './time.css'
 
 const Time = ({ time, colaboradores, deletarColaborador, mudaCor }) => {
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)' }}>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, 0.3)}}>
             <input type='color' className='muda-cor' value={time.cor} onChange={(evento) => mudaCor(evento.target.value, time.nome)} />
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
